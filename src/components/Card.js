c6b878bf5c71d46eac4f132c42061ed640dbf37c
aymenTM <t_mustapha_aymen@outlook.com>
2022-02-26 22:19:18 +0100

    Glow dice

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ import dvdesktop from '../images/pattern-divider-desktop.svg'
 function Card({advice,roldeDice,role}) {
 return (
 <div
-    className='max-w-md relative bg-[#313A49] rounded-lg shadow-lg flex flex-col items-center justify-center  p-4'>
+    className='max-w-md relative bg-[#313A49] rounded-lg shadow-lg flex flex-col items-center justify-center  p-4 transition duration-200'>
     <div className='flex flex-col items-center justify-center p-5 my-5 space-y-2'>
         <p className='text-[#52ffa8] tracking-[3px] font-semibold'>{`ADVICE #${advice['slip'].id}`}</p>
         <h1 className='text-[28px] text-white text-center p-2'>
@@ -14,7 +14,7 @@ return (
         <img className='hidden sm:block' src={dvdesktop} alt="divider" />
         <img className='sm:hidden' src={dvmobile} alt="divider" />
     </div>
-    <button onClick={()=>{roldeDice(!role)}} className=' absolute bottom-0 translate-y-1/2 bg-[#52ffa8] rounded-full
+    <button onClick={()=>{roldeDice(!role)}} className=' transition duration-200 hover:shadow-[0px_0px_20px_0px] hover:shadow-[#52ffa8] hover:shadow-2xl absolute bottom-0 translate-y-1/2 bg-[#52ffa8] rounded-full
         p-4'><img src={dice} alt="dice" /></button>
 </div>
 )
